5a1248af2d0d9fd85466040dddc4903058f775fb
DESKTOP-4JSDB9L\jpear <j.pearbach@yahoo.com>
2018-01-11 12:16:46 -0800

    Created the framework for the replay system.

---
+++ Assets/MyReplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MyKeyFrame testKey = new MyKeyFrame (Vector3.up, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+
+	public MyKeyFrame (Vector3 pos, Quaternion rot) {
+		frameTime = Time.time;
+		position = pos;
+		rotation = rot;
+	}
+
+}
